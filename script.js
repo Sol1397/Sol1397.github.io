86c4ff49c781f631027b46da2fbae241b2dc3a0b
Sol1397 <139805877+Sol1397@users.noreply.github.com>
2023-07-19 14:29:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,38 @@
 // Chave de API e segredo
 const apiKey = '8d207c60-4d6d-4468-867e-ed57237b7043';
-const apiSecret = '65de1f818be32aa7ffa5f666b699382eae58b36d6ea3549c4282a36b52d9f104f2ac877ce763e4f7040cec02dea75896d459645623d7b2deff71ff437c58adeb07eebcdd659c11581acfbd00b2a15a439f862b43f167acc1de9afac03871e94a0ab08e4e73d41525f99af381eb23fafc';
+const apiSecret = '65de1f818be32aa7ffa5f666b699382eae58b36d6ea3549c4282a36b52d9f104f2ac877ce763e4f7040cec02dea75896d459645623d7b2deff71ff437c58adeb07eebcdd659c11581acfbd00b2a15a439f862b43f167acc1de9afac03871e94a0ab08e4e73d41525f99af381eb23fafcm';
 
 // URL da API
-const apiUrl = 'https://api.astronomy.com/planet_position';
+const apiUrl = 'https://api.astronomyapi.com/v2/bodies';
 
-// Obter data atual
-const currentDate = new Date();
-const formattedDate = currentDate.toISOString().split('T')[0]; // Formato: AAAA-MM-DD
+// Elemento HTML para exibir os dados dos planetas
+const planetDataElement = document.getElementById('planet-data');
 
-// Elementos HTML
-const planetList = document.getElementById('planet-list');
-const moonPosition = document.getElementById('moon-position');
-const sunSign = document.getElementById('sun-sign');
+// Data inicial e final (formato: AAAA-MM-DD)
+const startDate = '2023-01-01';
+const endDate = '2023-01-07';
 
-// Função para fazer a solicitação à API
-async function fetchPlanetData() {
-    try {
-        // Montar a URL da solicitação com os parâmetros necessários
-        const url = `${apiUrl}?date=${formattedDate}&api_key=${apiKey}&api_secret=${apiSecret}`;
+// Fazer a solicitação à API para obter os dados dos planetas
+fetch(`${apiUrl}?start_date=${startDate}&end_date=${endDate}`, {
+  headers: {
+    'Authorization': `Basic ${btoa(`${apiKey}:${apiSecret}`)}`
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    // Processar os dados dos planetas
+    const planets = data.results;
+    let planetHtml = '';
 
-        // Fazer a solicitação GET à API
-        const response = await fetch(url);
-        const data = await response.json();
+    planets.forEach(planet => {
+      const sign = planet.sign ? planet.sign : 'Desconhecido';
 
-        // Extrair as informações dos planetas e do signo da resposta
-        const planets = data.planets;
-        const moon = data.moon;
-        const sun = data.sun;
+      planetHtml += `<p>${planet.name}: ${sign}</p>`;
+    });
 
-        // Atualizar a lista de planetas
-        planets.forEach(planet => {
-            const li = document.createElement('li');
-            li.textContent = `${planet.name}: ${planet.position} radianos`;
-            planetList.appendChild(li);
-        });
-
-        // Atualizar a posição da Lua
-        moonPosition.textContent = `Lua: ${moon.position} radianos`;
-
-        // Atualizar o signo do Sol
-        sunSign.textContent = `Signo: ${sun.sign}`;
-
-    } catch (error) {
-        console.error('Erro ao obter dados dos planetas:', error);
-    }
-}
-
-// Chamar a função para obter os dados dos planetas ao carregar a página
-fetchPlanetData();
+    // Exibir os dados dos planetas no elemento HTML
+    planetDataElement.innerHTML = planetHtml;
+  })
+  .catch(error => {
+    console.error('Erro ao obter dados dos planetas:', error);
+  });
