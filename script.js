951f5cd6452341d1c3c3f47418a8b55e4cef1362
Sol1397 <139805877+Sol1397@users.noreply.github.com>
2023-07-19 15:00:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// Chave de API
-const apiKey = '5e4c9b2f-9b2b-445e-8668-a991c41692d5';
+const fetch = require('node-fetch'); // Se estiver usando Node.js, instale o módulo "node-fetch" antes.
 
-// ID do corpo celeste (no caso da Lua é 'moon')
+// Chave de API
+const apiKey = '	5e4c9b2f-9b2b-445e-8668-a991c41692d5';
+
+// ID do corpo celeste (por exemplo, 'moon' para a Lua)
 const bodyId = 'moon';
 
 // URL da API
 const apiUrl = `https://api.astronomyapi.com/v2/bodies/${bodyId}`;
 
-// Fazer a solicitação à API para obter informações sobre a Lua
+// Fazer a solicitação à API para obter informações sobre o corpo celeste
 fetch(apiUrl, {
   headers: {
     'Authorization': `Bearer ${apiKey}`
