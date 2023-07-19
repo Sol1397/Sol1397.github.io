9738de87b9d596bc7b10c43c7ea17fcec3708198
Sol1397 <139805877+Sol1397@users.noreply.github.com>
2023-07-19 14:46:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Chave de API e segredo
-const apiKey = 'ca88e474-98fb-4236-a1b9-2aa52bb135b4';
-const apiSecret = '65de1f818be32aa7ffa5f666b699382eae58b36d6ea3549c4282a36b52d9f104f2ac877ce763e4f7040cec02dea75896d459645623d7b2deff71ff437c58adeb71a27785aa81eb9a1aaf4fee78e68985da2646cc631c490e83e05df1d3fa8fe125821e25690be5e36ba61291d945f2e5';
+const apiKey = 'abae3a74-f83c-47bc-92b5-c7d10aedcbdf';
+const apiSecret = '65de1f818be32aa7ffa5f666b699382eae58b36d6ea3549c4282a36b52d9f104f2ac877ce763e4f7040cec02dea75896d459645623d7b2deff71ff437c58adebd5a3f2ef2078e81c1a7450481ac1b18f4cc4c2aaa810bfe8926de0a80a75f52bbc6a42770eeae567747d2b73664264f6';
 
 // URL da API
 const apiUrl = 'https://api.astronomyapi.com/v2/signs';
