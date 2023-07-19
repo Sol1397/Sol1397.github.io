ff4fd22a8f8ef22539187b8b44e3d78c49b0a256
Sol1397 <139805877+Sol1397@users.noreply.github.com>
2023-07-19 14:37:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiKey = '8d207c60-4d6d-4468-867e-ed57237b7043';
 const apiSecret = '65de1f818be32aa7ffa5f666b699382eae58b36d6ea3549c4282a36b52d9f104f2ac877ce763e4f7040cec02dea75896d459645623d7b2deff71ff437c58adeb07eebcdd659c11581acfbd00b2a15a439f862b43f167acc1de9afac03871e94a0ab08e4e73d41525f99af381eb23fafcm';
 
 // URL da API
-const apiUrl = 'https://api.astronomyapi.com/v2/bodies';
+const apiUrl = 'https://api.astronomyapi.com/v2/signs';
 
 // Elemento HTML para exibir os dados dos planetas
 const planetDataElement = document.getElementById('planet-data');
@@ -20,19 +20,17 @@ fetch(`${apiUrl}?start_date=${startDate}&end_date=${endDate}`, {
 })
   .then(response => response.json())
   .then(data => {
-    // Processar os dados dos planetas
-    const planets = data.results;
+    // Processar os dados dos signos
+    const signs = data.signs;
     let planetHtml = '';
 
-    planets.forEach(planet => {
-      const sign = planet.sign ? planet.sign : 'Desconhecido';
-
-      planetHtml += `<p>${planet.name}: ${sign}</p>`;
+    signs.forEach(sign => {
+      planetHtml += `<p>${sign.planet}: ${sign.sign}</p>`;
     });
 
-    // Exibir os dados dos planetas no elemento HTML
+    // Exibir os dados dos signos no elemento HTML
     planetDataElement.innerHTML = planetHtml;
   })
   .catch(error => {
-    console.error('Erro ao obter dados dos planetas:', error);
+    console.error('Erro ao obter dados dos signos:', error);
   });
